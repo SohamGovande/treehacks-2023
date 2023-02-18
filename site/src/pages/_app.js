6f2d85e299fd589071d9c8aebc0e6e9124df105f
Soham Govande <vgsoham@gmail.com>
2023-02-17 22:26:31 -0800

    commit

--- site/src/pages/_app.js
+++ site/src/pages/_app.js
@@ -1,5 +1,8 @@
 import '@/styles/globals.css'
+import { ChakraProvider } from '@chakra-ui/react'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <ChakraProvider>
+    <Component {...pageProps} />
+  </ChakraProvider>
 }
