5f258d03c4bc4fc4ef584ef73fddf8a80cd78416
mcantillon21 <60069300+mcantillon21@users.noreply.github.com>
2023-02-18 17:02:38 -0800

    translation

--- site/src/pages/_app.js
+++ site/src/pages/_app.js
@@ -1,15 +1,24 @@
-import "@fontsource/raleway/400.css"
-import "@fontsource/raleway/700.css"
-import "@fontsource/inter/700.css"
-import "@fontsource/inter/400.css"
-import "@fontsource/nunito/400.css"
-import { ChakraProvider, defineStyleConfig, extendTheme } from "@chakra-ui/react"
-import "@/styles/globals.css"
+import '@fontsource/raleway/400.css'
+import '@fontsource/raleway/700.css'
+import '@fontsource/inter/700.css'
+import '@fontsource/inter/400.css'
+import '@fontsource/nunito/400.css'
+import {
+  ChakraProvider,
+  defineStyleConfig,
+  extendTheme,
+} from '@chakra-ui/react'
+import '@/styles/globals.css'
 
 const theme = extendTheme({
   fonts: {
     heading: "'Raleway', sans-serif",
     body: "'Nunito', sans-serif",
+  },
+  colors: {
+    black: {
+      500: '#000000',
+    },
   },
 })
 
