b18a4e5ce0ef28c10631ba6394bb5363371b29cf
Soham Govande <vgsoham@gmail.com>
2023-02-18 18:32:00 -0800

    push

--- site/src/components/MapPageContents.js
+++ site/src/components/MapPageContents.js
@@ -12,9 +12,10 @@ import {
   IconButton,
 } from "@chakra-ui/react"
 import Head from "next/head"
-import { lazy, useEffect, useState } from "react"
+import { lazy, useEffect, useRef, useState } from "react"
 import { AnimatePresence, motion } from "framer-motion"
 import { CloseIcon, LinkIcon } from "@chakra-ui/icons"
+import { Gradient } from "@/utils/gradient"
 
 const LazyMap = lazy(() => import("@/components/ReactMap"))
 
@@ -23,64 +24,69 @@ const gray = "#f1f1f1"
 const gradient = `linear-gradient(to bottom right, ${white}, ${gray}, ${white}, ${gray}, ${white}, ${gray})`
 
 const Sidebar = ({ dashboardId, setDashboardId }) => {
+  const canvasRef = useRef(null)
+  useEffect(() => {
+    const gradient = new Gradient()
+    gradient.initGradient("#gradient-canvas")
+  }, [])
+
   return (
     <AnimatePresence exitBeforeEnter>
-      {dashboardId !== -1 && (
-        <motion.div
-          transition={{
-            ease: "easeInOut",
-          }}
-          initial={{ x: "-100%", opacity: 0 }}
-          animate={{ x: "0%", opacity: 1 }}
-          exit={{ x: "-100%", opacity: 0 }}
-          style={{
-            position: "absolute",
-            top: 0,
-            left: 0,
-            bottom: 0,
-            width: "40%",
-            backgroundImage: "url('/hotspot.png')",
-            backgroundSize: "cover",
-            backgroundPosition: "center",
-            color: 'white'
-          }}>
-          <IconButton
-            position="absolute"
-            top={10}
-            right={8}
-            variant='unstyled'
-            aria-label="Close"
-            icon={<CloseIcon />}
-            onClick={() => setDashboardId(-1)}
-          />
-          <Box p={10} fontSize="xl">
-            <Heading as="h1">Hotspot #{dashboardId}</Heading>
-            <Text mt={4}>
-              Centered at <b>40.7128° N, 74.0060° W</b>
-            </Text>
-            <UnorderedList fontSize="xl" mt={4}>
-              <ListItem>
-                <b>Tracked data from dates:</b>&nbsp;2021-01-01 to 2021-01-31
-              </ListItem>
-              <ListItem>
-                <b>Estimated amount of wildlife loss:</b>&nbsp;1,500 fish
-              </ListItem>
-              <ListItem>
-                <b>Fish species threatened:</b>&nbsp;Salmon, Cod, Herring
-              </ListItem>
-            </UnorderedList>
-            <Heading as="h2" mt={4}>
-              Raw Data Points
-            </Heading>
-            [insert images here]
-            <Flex gap={2}>
-              <Button mt={4} colorScheme="blue" variant="solid" leftIcon={<LinkIcon />} onClick={() => onExport(id)}>
-                Export Report to Coast Guard
-              </Button>
-            </Flex>
-          </Box>
-        </motion.div>
-      )}
+      <motion.div
+        transition={{
+          ease: "easeInOut",
+        }}
+        initial={{ x: "-100%", opacity: 0 }}
+        animate={{ x: "0%", opacity: 1 }}
+        exit={{ x: "-100%", opacity: 0 }}
+        style={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          bottom: 0,
+          width: "40%",
+          color: "white",
+        }}>
+        <Box pos="absolute" top={0} left={0} w="100%" h="100%" zIndex={-2}>
+          <canvas ref={canvasRef} id="gradient-canvas" />
+        </Box>
+
+        <IconButton
+          position="absolute"
+          top={10}
+          right={8}
+          variant="unstyled"
+          aria-label="Close"
+          icon={<CloseIcon />}
+          onClick={() => setDashboardId(-1)}
+        />
+        <Box p={10} fontSize="xl">
+          <Heading as="h1">Hotspot #{dashboardId}</Heading>
+          <Text mt={4}>
+            Centered at <b>40.7128° N, 74.0060° W</b>
+          </Text>
+          <UnorderedList fontSize="xl" mt={4}>
+            <ListItem>
+              <b>Tracked data from dates:</b>&nbsp;2021-01-01 to 2021-01-31
+            </ListItem>
+            <ListItem>
+              <b>Estimated amount of wildlife loss:</b>&nbsp;1,500 fish
+            </ListItem>
+            <ListItem>
+              <b>Fish species threatened:</b>&nbsp;Salmon, Cod, Herring
+            </ListItem>
+          </UnorderedList>
+          <Heading as="h2" mt={4}>
+            Raw Data Points
+          </Heading>
+          [insert images here]
+          <Flex gap={2}>
+            <Button mt={4} colorScheme="blue" variant="solid" leftIcon={<LinkIcon />} onClick={() => onExport(id)}>
+              Export Report to Coast Guard
+            </Button>
+          </Flex>
+        </Box>
+      </motion.div>
     </AnimatePresence>
   )
 }
